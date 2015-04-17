f70fdad45b5fbcae2cbcbe95be3c28d87a7f3906
greg-sk <grzegorz.luczywo@securitykiss.com>
2015-04-17 12:35:52 +0200

    linuxdeps

--- sklib/linuxdeps.tcl
+++ sklib/linuxdeps.tcl
@@ -15,9 +15,31 @@ namespace eval ::linuxdeps {
 # Prefer feature detection over distro detection
 
 proc ::linuxdeps::find-pkg-mgr {} {
-# detect in order: zypper, apt-get, yum (others: pacman, portage, urpmi, dnf, slapt-geti emerge)
+  # detect in order: zypper, apt-get, yum (others: pacman, portage, urpmi, dnf, slapt-geti emerge)
+  set candidates {zypper apt-get yum}
+  foreach c $candidates {
+    if {![catch {exec $c --version}]} {
+      return $c
+    }
+  }
+  return ""
+}
+
+
+proc ::linuxdeps::tk-missing-dep {} {
+  catch {package require dksjfds} out err
+  puts "OUT: $out"
+  puts "ERR: $err"
+# When package missing:
+#OUT: can't find package dksjfds
+#ERR: -code 1 -level 0 -errorstack {INNER {invokeStk1 package require dksjfds} CALL tk-missing-dep} -errorcode {TCL PACKAGE UNFOUND} -errorinfo {can't find package dksjfds
+#    while executing
+#"package require dksjfds"} -errorline 2
+
 }
 
 proc ::linuxdeps::install {} {
-
+  set pkg [find-pkg-mgr]
+  puts $pkg
+  tk-missing-dep
 }
