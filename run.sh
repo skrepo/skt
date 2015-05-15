9b716616fdb2b36ef9b82b53c24613281c4698b6
greg-sk <grzegorz.luczywo@securitykiss.com>
2015-05-15 10:36:04 +0200

    run.sh

--- run.sh
+++ run.sh
@@ -3,9 +3,11 @@
 proc=`uname -p`
 
 if [ "$proc"="x86_64" ]; then
-    ./lib/linux-x86_64/base-tcl-8.6.3.1 bootstrap.tcl "$@"
+    cp ./lib/linux-x86_64/base-tcl-8.6.3.1 base-tcl
 else
-    ./lib/linux-ix86/base-tcl-8.6.3.1 bootstrap.tcl "$@"
+    cp ./lib/linux-ix86/base-tcl-8.6.3.1 base-tcl
 fi
 
+chmod +x base-tcl
+./base-tcl bootstrap.tcl "$@"
 
